b9083578c5cab39e367acf9f240c2ddf10ba46fc
juan-toto <78103035+juan-toto@users.noreply.github.com>
2025-01-04 15:58:04 -0300

    SQL server bloque 7

---
+++ SQL Server/7.3.sql
@@ -0,0 +1,2 @@
+-- 7.3. Informar la última orden generada por cada almacén con forma de pago a 30 días y 60 días.
+-- Mostrar código de almacén, fecha de la orden y forma de pago. Ordenar por fecha de orden.select a.almacen_id,max(v.fecha_orden) as 'ultima orden', v.forma_pago from almacenes as a, ventas as vwhere a.almacen_id = v.almacen_id and v.forma_pago like '%[30,60] días%'group by a.almacen_id , v.forma_pagoorder by 'ultima orden'	
