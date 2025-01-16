ad03af8f6c22f373cbdc97d54fc05e0f615dc70a
juan-toto <78103035+juan-toto@users.noreply.github.com>
2025-01-16 01:30:11 -0300

    SQL server bloque 8

---
+++ SQL Server/8.8.sql
@@ -0,0 +1,7 @@
+-- 8.8. ¿Informar cuantos títulos “Is Anger the Enemy?”  vendió cada almacén. Si un almacén 
+-- no tuvo ventas del mismo informar con un cero. Mostrar código de almacén y cantidad
+
+select a.almacen_id, isnull(sum(cantidad),0) as total
+from ventas as v inner join titulos as t on t.titulo_id = v.titulo_id and titulo = 'Is Anger the Enemy?' 
+				 right join almacenes as a on a.almacen_id = v.almacen_id
+group by a.almacen_id
